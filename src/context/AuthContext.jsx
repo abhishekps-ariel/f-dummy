24debaaff8416522260ee1e9a057dc12cab99fe1
abhishekps-ariel <abhishekp@arielsoftwares.in>
2025-10-13 10:12:18 +0530

    added eslint ignore in authContext

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -4,6 +4,7 @@ import { getAuthData } from '../utils/storage';
 
 const AuthContext = createContext();
 
+// eslint-disable-next-line react-refresh/only-export-components
 export const useAuth = () => {
   const context = useContext(AuthContext);
   if (!context) {
